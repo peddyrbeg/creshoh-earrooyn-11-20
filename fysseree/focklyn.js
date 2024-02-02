f6c74fdd54632bf717bbe08af95365be8df79eb0
peddyrbeg <85888409+peddyrbeg@users.noreply.github.com>
2024-02-02 21:09:26 +0000

    Add files via upload

---
+++ fysseree/focklyn.js
@@ -0,0 +1,42 @@
+const focklynYnsit = {
+    "nane-jeig": {
+        caslys: "caslyssyn/nane-jeig.webp",
+        sheean: "sheeanyn/nane-jeig.mp3"
+    },
+    "daa-yeig": {
+        caslys: "caslyssyn/daa-yeig.webp",
+        sheean: "sheeanyn/daa-yeig.mp3"
+    },
+    "tree-jeig": {
+        caslys: "caslyssyn/tree-jeig.webp",
+        sheean: "sheeanyn/tree-jeig.mp3"
+    },
+    "kiare-jeig": {
+        caslys: "caslyssyn/kiare-jeig.webp",
+        sheean: "sheeanyn/kiare-jeig.mp3"
+    },
+    "queig-jeig": {
+        caslys: "caslyssyn/queig-jeig.webp",
+        sheean: "sheeanyn/queig-jeig.mp3"
+    },
+    "shey-jeig": {
+        caslys: "caslyssyn/shey-jeig.webp",
+        sheean: "sheeanyn/shey-jeig.mp3"
+    },
+    "shiaght-jeig": {
+        caslys: "caslyssyn/shiaght-jeig.webp",
+        sheean: "sheeanyn/shiaght-jeig.mp3"
+    },
+    "hoght-jeig": {
+        caslys: "caslyssyn/hoght-jeig.webp",
+        sheean: "sheeanyn/hoght-jeig.mp3"
+    },
+    "nuy-jeig": {
+        caslys: "caslyssyn/nuy-jeig.webp",
+        sheean: "sheeanyn/nuy-jeig.mp3"
+    },
+    "feed": {
+        caslys: "caslyssyn/feed.webp",
+        sheean: "sheeanyn/feed.mp3"
+    }
+};
